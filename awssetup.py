7727c983e99ca4c1fd0315227bca9b84f482b1ad
unknown <22ht1a6137@gmail.com>
2025-02-17 18:51:55 +0530

    aws

--- awssetup.py
+++ awssetup.py
@@ -1,1 +1,6 @@
-print("hello world")
+create new repository named motion-recruitment
+new branch named feature-aws-setup 
+create python file in feature-aws-setup branch
+create readme file in main branch
+push those two files in branches
+merge the files
